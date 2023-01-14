c30e124630697958bc21834133effa30c9f94650
Gonçalo Galante <72885029+goncalogalante@users.noreply.github.com>
2023-01-14 10:45:54 +0000

    Homework 2 final 1

--- models.py
+++ models.py
@@ -47,13 +47,33 @@ class Attention(nn.Module):
         # - Use torch.tanh to do the tanh
         # - Use torch.masked_fill to do the masking of the padding tokens
         #############################################
-        raise NotImplementedError
+        
+        z = self.linear_in(query) # [64, 1, 128] -> [batchsize, 1, hidden_dim]
+
+        h_i = encoder_outputs # [64, 19, 128] -> [batchsize, max_src_lenght, hidden_dim]  
+        
+        s_i = torch.bmm(z, h_i.transpose(1, 2)) # [64, 1, 128] x [64, 128, 19] = [64, 1, 19]
+
+        s_i = torch.masked_fill(s_i, src_seq_mask.unsqueeze(1), float("-inf")) # [64, 1, 19]
+        
+        p_i = torch.softmax(s_i, dim=-1) # [64, 1, 19]
+        
+        context_vector = torch.bmm(p_i,encoder_outputs) # [64, 1, 19] x [64, 19, 128] = [64, 1, 128]
+        
+        attn_out = torch.cat((query, context_vector), dim=2) # [64, 1, 256]
+
+        attn_out = self.linear_out(attn_out) # [64, 1, 128]
+        
+        attn_out = torch.tanh(attn_out) # [64, 1, 128]
+        
+        # raise NotImplementedError
+        
         #############################################
         # END OF YOUR CODE
         #############################################
         # attn_out: (batch_size, 1, hidden_size)
         # TODO: Uncomment the following line when you implement the forward pass
-        # return attn_out
+        return attn_out
 
     def sequence_mask(self, lengths):
         """
@@ -109,7 +129,19 @@ class Encoder(nn.Module):
         # - Use torch.nn.utils.rnn.pad_packed_sequence to unpack the packed sequences
         #   (after passing them to the LSTM)
         #############################################
-        raise NotImplementedError
+        
+        src_emb = self.dropout(self.embedding(src))
+        
+        pack_input = pack(input = src_emb, lengths= lengths, batch_first=True, enforce_sorted=False)
+        
+        pack_output, final_hidden = self.lstm(input = pack_input)
+    
+        enc_output, lenghts_unpacked = unpack(sequence = pack_output, batch_first=True)
+        
+        enc_output = self.dropout(enc_output)
+        
+        # raise NotImplementedError
+        
         #############################################
         # END OF YOUR CODE
         #############################################
@@ -117,7 +149,7 @@ class Encoder(nn.Module):
         # final_hidden: tuple with 2 tensors
         # each tensor is (num_layers * num_directions, batch_size, hidden_size)
         # TODO: Uncomment the following line when you implement the forward pass
-        # return enc_output, final_hidden
+        return enc_output, final_hidden
 
 
 class Decoder(nn.Module):
@@ -180,7 +212,30 @@ class Decoder(nn.Module):
         #         src_lengths,
         #     )
         #############################################
-        raise NotImplementedError
+        
+        # if its in the train mode the size is greater than 1 so we slice the <END> token.
+        if tgt.size(1)>1:
+            tgt = tgt[:, :-1]
+            
+        tgt_emb = self.dropout(self.embedding(tgt)) # tgt_emb train: [64, 20, 128]; test: [1, 1, 128]
+        
+        outputs = torch.ones(tgt.shape[0], tgt.shape[1], self.hidden_size)
+       
+        for t in range(tgt.shape[1]):
+    
+            input_t = tgt_emb[:,t,:] # input_t: train [64, 128]; test [1, 128]
+            
+            output, dec_state = self.lstm(input_t.unsqueeze(1), dec_state)
+            
+            if self.attn is not None:
+                output = self.attn(output, encoder_outputs, src_lengths)     
+                
+            output = self.dropout(output)
+        
+            outputs[:,t,:] = output.squeeze(1) # outputs: train [64, 20, 128]; test [1, 1, 128]
+                
+        # raise NotImplementedError
+        
         #############################################
         # END OF YOUR CODE
         #############################################
@@ -188,7 +243,7 @@ class Decoder(nn.Module):
         # dec_state: tuple with 2 tensors
         # each tensor is (num_layers, batch_size, hidden_size)
         # TODO: Uncomment the following line when you implement the forward pass
-        # return outputs, dec_state
+        return outputs, dec_state
 
 
 class Seq2Seq(nn.Module):
